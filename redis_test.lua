09db5e7fbbad7f8c3be064bba3ab879d249bc1db
EamonYi <bigsmile_wb@126.com>
2017-02-19 22:16:29 +0800

    eval success

--- redis_test.lua
+++ redis_test.lua
@@ -7,10 +7,10 @@ if nil == key or "" == key then
 end
 
 local incr_str = 'local exist_key = redis.call("exists", KEYS[1])\n' ..
-'if tonumber(exist_key) == 1 then\n' ..
-'    return redis.call("incr", KEYS[1])\n' ..
-'end\n' ..
-'return 0'
+                 'if tonumber(exist_key) == 1 then\n' ..
+                 '    return redis.call("incr", KEYS[1])\n' ..
+                 'end\n' ..
+                 'return 0'
 
 ngx.say(redisutil.cmd('eval',incr_str, 1, key))
 redisutil.pipline() 
