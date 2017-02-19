109df2135f1d2f8948dec544c4ea96c3e17d5aa9
EamonYi <bigsmile_wb@126.com>
2017-02-19 22:03:32 +0800

    args

--- redis_test.lua
+++ redis_test.lua
@@ -1,3 +1,11 @@
+
+local args = ngx.req.get_uri_args()
+local key = args.key 
+
+if nil == key or "" == key then
+    ngx.exit(406)
+end
+
 local incr_str = '
 local exist_key = redis.call("exists", KEYS[1])
 if tonumber(exist_key) == 1 then
@@ -6,5 +14,5 @@ end
 return 0
 '
 
-ngx.say(redisutil.cmd('eval','incr_str','123'))
+ngx.say(redisutil.cmd('eval',incr_str,'123'))
 redisutil.pipline() 
