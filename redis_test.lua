c32f5c5359cfbda592d3263252368a494fa08e29
EamonYi <bigsmile_wb@126.com>
2017-02-19 22:00:51 +0800

    eval

--- redis_test.lua
+++ redis_test.lua
@@ -1,2 +1,10 @@
--- ngx.say(redisutil.cmd('set','test','123'))
+local incr_str = '
+local exist_key = redis.call("exists", KEYS[1])
+if tonumber(exist_key) == 1 then
+    return redis.call("incr", KEYS[1])
+end
+return 0
+'
+
+ngx.say(redisutil.cmd('eval','incr_str','123'))
 redisutil.pipline() 
