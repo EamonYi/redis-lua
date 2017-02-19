32bf5f69131aff66954d46e95e2aac8324ff9c09
EamonYi <bigsmile_wb@126.com>
2017-02-19 22:08:16 +0800

    lua string

--- redis_test.lua
+++ redis_test.lua
@@ -6,13 +6,11 @@ if nil == key or "" == key then
     ngx.exit(406)
 end
 
-local incr_str = '
-local exist_key = redis.call("exists", KEYS[1])
-if tonumber(exist_key) == 1 then
-    return redis.call("incr", KEYS[1])
-end
-return 0
-'
+local incr_str = 'local exist_key = redis.call("exists", KEYS[1])\n' ..
+'if tonumber(exist_key) == 1 then\n' ..
+'    return redis.call("incr", KEYS[1])\n' ..
+'end\n' ..
+'return 0'
 
 ngx.say(redisutil.cmd('eval',incr_str,'123'))
 redisutil.pipline() 
